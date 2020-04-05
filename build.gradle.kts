7cd8ee7a2f9b2e53e23ccb9b07a3191171db5477
Nick Robison <nick@nickrobison.com>
2020-04-05 16:47:55 -0400

    Signing and deployment seems to be working.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,15 @@
 val janinoVersion by extra("3.1.0")
+plugins {
+    signing
+    `maven-publish`
+}
 
 allprojects {
     group = "com.nickrobison"
     version = "0.3.2-SNAPSHOT"
 
     apply(plugin = "java")
+    apply(plugin = "signing")
 
     repositories {
         jcenter()
@@ -21,4 +26,32 @@ allprojects {
         sourceCompatibility = "1.8"
         targetCompatibility = "1.8"
     }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+
+        }
+    }
+
+    repositories {
+        maven {
+            credentials {
+                val bintrayUsername: String by project
+                val bintrayPassword: String by project
+                username = bintrayUsername
+                password = bintrayPassword
+            }
+            val releasesRepoUrl = "https://api.bintray.com/maven/nickrobison/maven/fasttuple/;publish=1"
+            val snapshotsRepoUrl = "http://oss.jfrog.org/artifactory/oss-snapshot-local"
+            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+            name = "bintray-nickrobison-maven"
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications["mavenJava"])
 }
