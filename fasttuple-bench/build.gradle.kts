fd063b658fe252e40bfc4b232ca3a8da010e21f2
Nick Robison <nick@nickrobison.com>
2020-04-05 16:08:42 -0400

    Refactored common dependencies.
    Janino and Guava are now root dependencies.
    
    Updated Guava to an older dependency version, to see if Synk catches it.
    
    Setup extra properties to pass down to the sub modules.

--- fasttuple-bench/build.gradle.kts
+++ fasttuple-bench/build.gradle.kts
@@ -1,11 +1,9 @@
 val jmhVersion = "1.23"
 
 dependencies {
+    implementation(project(":fasttuple-core"))
     implementation("org.openjdk.jmh:jmh-core:$jmhVersion")
     implementation("nf.fr.eraasoft:objectpool:1.1.2")
-    implementation("com.google.guava:guava:23.0")
-    implementation("org.codehaus.janino:janino:3.1.0")
-    implementation(project(":fasttuple-core"))
     annotationProcessor("org.openjdk.jmh:jmh-generator-annprocess:$jmhVersion")
 }
 
--- fasttuple-core/build.gradle.kts
+++ fasttuple-core/build.gradle.kts
@@ -1,10 +1,7 @@
-val janinoVersion = "3.1.0"
 val junitVersion = "5.6.1"
 
 dependencies {
-    implementation("org.codehaus.janino:janino:$janinoVersion")
-    implementation("com.google.guava:guava:23.0")
-    implementation("org.codehaus.janino:commons-compiler:$janinoVersion")
+    implementation("org.codehaus.janino:commons-compiler:${rootProject.ext.get("janinoVersion")}")
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
 }
